9ccb7db36fb2cae89f8130311a4ed62bf41247d8
Hicham Benfettouma <hibenfet@e-r8-p3.s19.be>
2019-11-26 11:40:54 +0100

    strlcat reparer

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: hibenfet <hibenfet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 19:21:02 by hibenfet          #+#    #+#             */
-/*   Updated: 2019/11/25 17:24:55 by hibenfet         ###   ########.fr       */
+/*   Updated: 2019/11/26 11:38:35 by hibenfet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,36 +37,48 @@ size_t			ft_strlcat(char *dst, const char *src, size_t size)
 {
 	unsigned int i;
 	unsigned int j;
-	//unsigned int tot;
-	size_t dstlen;
+	unsigned int tot;
 
 
-	if (!src || (!dst && size != 0))
-		return (0);
-	if (!dst && size == 0)
+	/*if (!src || (!dst && size != 0))
+		return (0);*/
+	if (!dst && size == 0 && *src)
 		return (ft_strlen(src));
-	dstlen = ft_strlen(dst);
 	i = 0;
 	j = 0;
-	while (dst[j])
-		j++;
-	while (src[i] && i < size - 1)
+	while (dst[j] && j < size)
 	{
-		dst[j++] = src[i++];
+		printf("j == %d < size : %zu\n", j, size);
+		j++;
+	}
+	while (src[i])
+	{
+		printf("src[i] == %c pour i == %d\n", src[i], i);
+		i++;
+	}
+	tot = i + j;
+	printf("tot == %d\n", tot);
+	/*if (size == i)
+		return (tot);*/
+	i = 0;
+	while (src[i] && j + i < size - 1)
+	{
+		dst[j + i] = src[i];
+		i++;
 	}
 	dst[j] = '\0';
 	printf("concat impli == %s\n", dst);
-	return (j);
+	return (tot);
 }
 
 int		main(void)
 {
-	char dst[20] = "je m appele";
-	char *src = "NULL555";
-	char *dst1 = "je m appele";
-	char *src1 = "NULL555";
-	printf("%zu\n", ft_strlcat(dst, src, 5));
+	char dst[20] = "NULLfdf";
+	char *src = "NULL5fsadf";
+	char dst1[20] = "NULLfdf";
+	char *src1 = "NULL5fsadf";
+	printf("%zu\n", ft_strlcat(dst, src, 20));
 	//printf("dest vaut %s\n", dst);
-	printf("%zu\n", strlcat(dst1, src1, 5));
+	printf("%zu\n", strlcat(dst1, src1, 20));
 	//printf("dest vaut %s\n", dst1);
 }
